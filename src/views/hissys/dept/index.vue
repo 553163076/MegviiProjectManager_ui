87b294703c65a0c3f24267b6ae5241a5732350aa
“xcy55@bupt.edu.cn” <xiaochunyang@megvii.com>
2022-02-18 16:45:12 +0800

    feat:新增科室管理

--- src/views/hissys/dept/index.vue
+++ src/views/hissys/dept/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryForm" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="科室名称" prop="postName">
+      <el-form-item label="科室名称" prop="deptName">
         <el-input
           v-model="queryParams.deptName"
           placeholder="请输入科室名称"
@@ -10,14 +10,45 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
+      <el-form-item label="科室编码" prop="deptCode">
+        <el-input
+          v-model="queryParams.deptCode"
+          placeholder="请输入科室编码"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="当前挂号量" prop="deptNum">
+        <el-input
+          v-model="queryParams.deptNum"
+          placeholder="请输入当前挂号量"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="负责人" prop="deptLeader">
+        <el-input
+          v-model="queryParams.deptLeader"
+          placeholder="请输入负责人"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="手机号码" prop="deptPhone">
+        <el-input
+          v-model="queryParams.deptPhone"
+          placeholder="请输入手机号码"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
       <el-form-item label="状态" prop="status">
-        <el-select v-model="queryParams.status" placeholder="科室状态" clearable size="small">
-          <el-option
-            v-for="dict in statusOptions"
-            :key="dict.dictValue"
-            :label="dict.dictLabel"
-            :value="dict.dictValue"
-          />
+        <el-select v-model="queryParams.status" placeholder="请选择状态" clearable size="small">
+          <el-option label="请选择字典生成" value=""/>
         </el-select>
       </el-form-item>
       <el-form-item>
@@ -35,7 +66,8 @@
           size="mini"
           @click="handleAdd"
           v-hasPermi="['hissys:dept:add']"
-        >新增</el-button>
+        >新增
+        </el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -46,7 +78,8 @@
           :disabled="single"
           @click="handleUpdate"
           v-hasPermi="['hissys:dept:edit']"
-        >修改</el-button>
+        >修改
+        </el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -57,7 +90,8 @@
           :disabled="multiple"
           @click="handleDelete"
           v-hasPermi="['hissys:dept:remove']"
-        >删除</el-button>
+        >删除
+        </el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -67,25 +101,26 @@
           size="mini"
           @click="handleExport"
           v-hasPermi="['hissys:dept:export']"
-        >导出</el-button>
+        >导出
+        </el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="deptList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="科室ID" align="center" prop="deptId" />
-      <el-table-column label="科室名称" align="center" prop="deptName" />
-      <el-table-column label="科室编码" align="center" prop="deptCode" />
-      <el-table-column label="挂号量" align="center" prop="no" />
-      <el-table-column label="负责人" align="center" prop="deptCharge" />
-      <el-table-column label="电话" align="center" prop="phone" />
-      <el-table-column label="状态" align="center" prop="status" :formatter="statusFormat" />
-      <el-table-column label="创建时间" align="center" prop="createTime" width="180">
+      <el-table-column type="selection" width="55" align="center"/>
+      <el-table-column label="科室ID" align="center" prop="deptId"/>
+      <el-table-column label="科室名称" align="center" prop="deptName"/>
+      <el-table-column label="科室编码" align="center" prop="deptCode"/>
+      <el-table-column label="当前挂号量" align="center" prop="deptNum"/>
+      <el-table-column label="负责人" align="center" prop="deptLeader"/>
+      <el-table-column label="手机号码" align="center" prop="deptPhone"/>
+      <el-table-column label="状态" align="center" prop="status">
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.createTime) }}</span>
+          <span>{{ scope.row.status == 1 ? '停用' : '正常' }}</span>
         </template>
       </el-table-column>
+      <el-table-column label="备注" align="center" prop="remark"/>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button
@@ -94,14 +129,16 @@
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
             v-hasPermi="['hissys:dept:edit']"
-          >修改</el-button>
+          >修改
+          </el-button>
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['hissys:dept:remove']"
-          >删除</el-button>
+          >删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -118,28 +155,27 @@
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="科室名称" prop="deptName">
-          <el-input v-model="form.deptName" placeholder="请输入科室名称" />
+          <el-input v-model="form.deptName" placeholder="请输入科室名称"/>
         </el-form-item>
         <el-form-item label="科室编码" prop="deptCode">
-          <el-input v-model="form.deptCode" placeholder="请输入编码名称" />
+          <el-input v-model="form.deptCode" placeholder="请输入科室编码"/>
         </el-form-item>
-        <el-form-item label="负责人" prop="deptCharge">
-          <el-input v-model="form.deptCharge" placeholder="请输入负责人" />
+        <el-form-item label="当前挂号量" prop="deptNum">
+          <el-input v-model="form.deptNum" placeholder="请输入当前挂号量"/>
         </el-form-item>
-        <el-form-item label="电话" prop="phone">
-          <el-input v-model="form.phone" placeholder="请输入电话" />
+        <el-form-item label="负责人" prop="deptLeader">
+          <el-input v-model="form.deptLeader" placeholder="请输入负责人"/>
         </el-form-item>
-        <el-form-item label="科室顺序" prop="orderNum">
-          <el-input-number v-model="form.orderNum" controls-position="right" :min="0" />
+        <el-form-item label="手机号码" prop="deptPhone">
+          <el-input v-model="form.deptPhone" placeholder="请输入手机号码"/>
         </el-form-item>
-        <el-form-item label="科室状态" prop="status">
+        <el-form-item label="状态">
           <el-radio-group v-model="form.status">
-            <el-radio
-              v-for="dict in statusOptions"
-              :key="dict.dictValue"
-              :label="dict.dictValue"
-            >{{dict.dictLabel}}</el-radio>
+            <el-radio label="1">请选择字典生成</el-radio>
           </el-radio-group>
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容"/>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -151,10 +187,11 @@
 </template>
 
 <script>
-import { listDept, getPost, delPost, addDept, updateDept, exportPost } from "@/api/hissys/dept";
+import { listDept, getDept, delDept, addDept, updateDept, exportDept } from '@/api/hissys/dept'
 
 export default {
-  name: "Dept",
+  name: 'Dept',
+  components: {},
   data() {
     return {
       // 遮罩层
@@ -172,146 +209,151 @@ export default {
       // 科室表格数据
       deptList: [],
       // 弹出层标题
-      title: "",
+      title: '',
       // 是否显示弹出层
       open: false,
-      // 状态数据字典
-      statusOptions: [],
       // 查询参数
       queryParams: {
         pageNum: 1,
         pageSize: 10,
-        deptName: undefined,  // 科室名称
-        status: undefined // 状态
+        deptName: null,
+        deptCode: null,
+        deptNum: null,
+        deptLeader: null,
+        deptPhone: null,
+        status: null
       },
       // 表单参数
       form: {},
       // 表单校验
       rules: {
         deptName: [
-          { required: true, message: "科室名称不能为空", trigger: "blur" }
+          { required: true, message: '科室名称不能为空', trigger: 'blur' }
+        ],
+        deptCode: [
+          { required: true, message: '科室编码不能为空', trigger: 'blur' }
+        ],
+        status: [
+          { required: true, message: '状态不能为空', trigger: 'blur' }
         ]
       }
-    };
+    }
   },
   created() {
-    this.getList();
-    this.getDicts("sys_normal_disable").then(response => {
-      this.statusOptions = response.data;
-    });
+    this.getList()
   },
   methods: {
     /** 查询科室列表 */
     getList() {
-      this.loading = true;
+      this.loading = true
       listDept(this.queryParams).then(response => {
-        this.deptList = response.rows;
-        this.total = response.total;
-        this.loading = false;
-      });
-    },
-    // 科室状态字典翻译
-    statusFormat(row, column) {
-      return this.selectDictLabel(this.statusOptions, row.status);
+        this.deptList = response.rows
+        this.total = response.total
+        this.loading = false
+      })
     },
     // 取消按钮
     cancel() {
-      this.open = false;
-      this.reset();
+      this.open = false
+      this.reset()
     },
     // 表单重置
     reset() {
       this.form = {
-        deptId: undefined,
-        deptCode: undefined,
-        deptName: undefined,
-        deptCharge: undefined,
-        orderNum: 0,
-        phone: undefined,
-        status: "0",
-        remark: undefined
-      };
-      this.resetForm("form");
+        deptId: null,
+        deptName: null,
+        deptCode: null,
+        deptNum: null,
+        deptLeader: null,
+        deptPhone: null,
+        status: '0',
+        createBy: null,
+        createTime: null,
+        updateBy: null,
+        updateTime: null,
+        remark: null
+      }
+      this.resetForm('form')
     },
     /** 搜索按钮操作 */
     handleQuery() {
-      this.queryParams.pageNum = 1;
-      this.getList();
+      this.queryParams.pageNum = 1
+      this.getList()
     },
     /** 重置按钮操作 */
     resetQuery() {
-      this.resetForm("queryForm");
-      this.handleQuery();
+      this.resetForm('queryForm')
+      this.handleQuery()
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
       this.ids = selection.map(item => item.deptId)
-      this.single = selection.length!=1
+      this.single = selection.length !== 1
       this.multiple = !selection.length
     },
     /** 新增按钮操作 */
     handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加科室";
+      this.reset()
+      this.open = true
+      this.title = '添加科室'
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
-      this.reset();
-      const postId = row.deptId || this.ids
-      getPost(postId).then(response => {
-        this.form = response.data;
-        this.open = true;
-        this.title = "修改科室";
-      });
+      this.reset()
+      const deptId = row.deptId || this.ids
+      getDept(deptId).then(response => {
+        this.form = response.data
+        this.open = true
+        this.title = '修改科室'
+      })
     },
     /** 提交按钮 */
-    submitForm: function() {
-      this.$refs["form"].validate(valid => {
+    submitForm() {
+      this.$refs['form'].validate(valid => {
         if (valid) {
-          if (this.form.deptId != undefined) {
+          if (this.form.deptId != null) {
             updateDept(this.form).then(response => {
-              this.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
+              this.msgSuccess('修改成功')
+              this.open = false
+              this.getList()
+            })
           } else {
             addDept(this.form).then(response => {
-              this.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
+              this.msgSuccess('新增成功')
+              this.open = false
+              this.getList()
+            })
           }
         }
-      });
+      })
     },
     /** 删除按钮操作 */
     handleDelete(row) {
-      const postIds = row.deptId || this.ids;
-      this.$confirm('是否确认删除科室编号为"' + postIds + '"的数据项?', "警告", {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning"
-        }).then(function() {
-          return delPost(postIds);
-        }).then(() => {
-          this.getList();
-          this.msgSuccess("删除成功");
-        })
+      const deptIds = row.deptId || this.ids
+      this.$confirm('是否确认删除科室编号为"' + deptIds + '"的数据项?', '警告', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(function() {
+        return delDept(deptIds)
+      }).then(() => {
+        this.getList()
+        this.msgSuccess('删除成功')
+      })
     },
     /** 导出按钮操作 */
     handleExport() {
-      const queryParams = this.queryParams;
-      this.$confirm('是否确认导出所有科室数据项?', "警告", {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning"
-        }).then(function() {
-          return exportPost(queryParams);
-        }).then(response => {
-          this.download(response.msg);
-        })
+      const queryParams = this.queryParams
+      this.$confirm('是否确认导出所有科室数据项?', '警告', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(function() {
+        return exportDept(queryParams)
+      }).then(response => {
+        this.download(response.msg)
+      })
     }
   }
-};
+}
 </script>
